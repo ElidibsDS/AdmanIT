c5dfb058edbd76d5c54dedbb5f63a739aaec66b3
Elidibs <nickutho@gmail.com>
2019-05-02 17:46:46 -0500

    init

---
+++ Backend/Backend/Helpers/ServiceHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.Helpers
+{
+    public class ServiceHelper<T>
+    {
+        public ServiceHelper()
+        {
+        }
+    }
+}
---
+++ Backend/Backend/Models/Pagination.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.Models
+{
+    public class Pagination
+    {
+    }
+}
